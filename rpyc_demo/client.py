aa613fd50787e968c317c504179397f7511d6d83
Charlie <liuyangcharlie@gmail.com>
2019-12-14 18:21:07 +0000

    add client to new threads

--- rpyc_demo/client.py
+++ rpyc_demo/client.py
@@ -1,5 +1,11 @@
 import rpyc
+import threading
 
-c = rpyc.connect("172.17.0.2", 18861)
-c.root.get_answer()
+def connect_and_call():
+    c = rpyc.connect("172.17.0.2", 18861)
+    print(c.root.get_answer())
 
+t = threading.Thread(target=connect_and_call)
+t.start()
+print('client start...')
+input('any key')
